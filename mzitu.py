eb1114ba8d34d77e401cc23f4aff610e07c13fa4
wzyonggege <jamwong994@gmail.com>
2017-08-06 12:29:09 +0800

    update headers

--- mzitu.py
+++ mzitu.py
@@ -4,6 +4,20 @@ from lxml import html
 import os
 import time
 
+def header(referer):
+    headers = {
+        'Host': 'i.meizitu.net',
+        'Pragma': 'no-cache',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/59.0.3071.115 Safari/537.36',
+        'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
+        'Referer': '{}'.format(referer),
+    }
+    return headers
 
 # 获取主页列表
 def getPage(pageNum):
@@ -42,7 +56,7 @@ def getPiclink(url):
             filename = '%s/%s/%s.jpg' % (os.path.abspath('.'), dirName, n)
             print(u'开始下载图片:%s 第%s张' % (dirName, n))
             with open(filename, "wb+") as jpg:
-                jpg.write(requests.get(jpgLink).content)
+                jpg.write(requests.get(jpgLink, headers=header(jpgLink)).content)
             n += 1
         except:
             pass
